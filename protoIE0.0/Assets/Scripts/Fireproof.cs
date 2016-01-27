cef73886b8fc63459eca80d8caf60020597ee18e
Frédéric Carrier <dragon_eyes@live.fr>
2016-01-27 12:00:00 -0500

    Initial commit with test zone for fire.

---
+++ protoIE0.0/Assets/Scripts/Fireproof.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Fireproof : MonoBehaviour
+{
+	public bool flamable;
+	private bool onFire;
+
+    //Hp would be in the stats script of the game object
+    private int hp = 10;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{   
+        if (hp <= 0)
+        {
+            //Kill the object
+            Destroy(gameObject);
+        }
+	}
+
+	public bool isFlamable()
+	{
+		return flamable;
+	}
+
+	public void setIsOnFire(bool newOnFire)
+	{
+		onFire = newOnFire;
+
+        if(onFire)
+        {
+            InvokeRepeating("dealDamage", 0, 0.5f);
+        }
+	}
+
+    public void dealDamage()
+    {
+        hp -= 1;
+    }
+
+	public bool isOnFire()
+	{
+		return onFire;
+	}
+}
