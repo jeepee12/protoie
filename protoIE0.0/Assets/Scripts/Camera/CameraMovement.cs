44bcd269a5c7af86bc8b65cbfea6fc72ac1842cc
bluespart <olivier.allard93@gmail.com>
2016-02-08 01:53:29 -0500

    Small change to the camera behavior Presets are available for the camera position. Try which you prefer.

--- protoIE0.0/Assets/Scripts/Camera/CameraMovement.cs
+++ protoIE0.0/Assets/Scripts/Camera/CameraMovement.cs
@@ -6,13 +6,39 @@ public class CameraMovement : MonoBehaviour
 
     public GameObject player;
 
+    public int CameraPreset = 1;
+
+    private Vector3 CameraPreset1Position = new Vector3(0.0f, 150.0f, -150.0f);
+    private Vector3 CameraPreset1Rotation = new Vector3(45.0f, 0.0f, 0.0f);
+
+    private Vector3 CameraPreset2Position = new Vector3(0.0f, 150.0f, 0.0f);
+    private Vector3 CameraPreset2Rotation = new Vector3(90.0f, 0.0f, 0.0f);
+
     private Vector3 offset;
 
     void Start()
     {
+
+        switch (CameraPreset)
+        {
+            case 1:
+                transform.position = CameraPreset1Position;
+                transform.eulerAngles = CameraPreset1Rotation;
+                break;
+
+            case 2:
+                transform.position = CameraPreset2Position;
+                transform.eulerAngles = CameraPreset2Rotation;
+                break;
+
+            default:
+                transform.position = CameraPreset1Position;
+                transform.eulerAngles = CameraPreset1Rotation;
+                break;
+
+        }
+
         offset = transform.position - player.transform.position;
-        /*if (player.GetComponent<PlayerMovement>())
-            player.GetComponent<PlayerMovement>().movement;*/
     }
 
     void LateUpdate()
