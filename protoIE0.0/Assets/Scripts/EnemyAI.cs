f5453e8dc47d15981f190b4cafb82099b05d04bc
Baltazar510 <devil.hunter510@gmail.com>
2016-02-08 19:28:25 -0500

    Add Basic Enemy AI
    Add the navigation scene to AI Scene
    Add an enemy AI

--- protoIE0.0/Assets/Scripts/EnemyAI.cs
+++ protoIE0.0/Assets/Scripts/EnemyAI.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    public float lookAtDistance = 100f;
+    public float chaseAtDistance = 30f;
+    public float attackRange = 10f;
+    public float speed = 16f;
+    public float damping = 0.8f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private float playerDistance;
+    private NavMeshAgent navAgent;
+
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        playerDistance = Vector3.Distance(target.position, transform.position);
+
+        if (playerDistance > lookAtDistance)
+        {
+            // Chill for the moment
+        } else
+        {
+            LookAt();
+        }
+        
+        if (playerDistance <= attackRange)
+        {
+            navAgent.Stop();
+            // Enemy do an attack
+        }
+        else if (playerDistance <= chaseAtDistance)
+        {
+            navAgent.Resume();
+            Move();
+        }
+    }
+
+    private void LookAt()
+    {
+        // Rotate to look at player.
+        Quaternion turnRotation = Quaternion.LookRotation(target.position - transform.position);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, turnRotation, Time.deltaTime * damping);
+    }
+
+    private void Move()
+    {
+        navAgent.destination = target.position;
+    }
+
+
 }
