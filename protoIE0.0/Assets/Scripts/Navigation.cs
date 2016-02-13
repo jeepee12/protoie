90f623d7b62e191e68fba7054ac8604e456e442c
jeepee12 <jeepee.12@hotmail.com>
2016-02-13 14:26:42 -0500

    Ajout de la possibilité de reculer avec le bouton B sur une manette de Xbox

--- protoIE0.0/Assets/Scripts/Navigation.cs
+++ protoIE0.0/Assets/Scripts/Navigation.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Navigation : MonoBehaviour {
+public class Navigation : MonoBehaviour
+{
 
     private Vector3 m_ProgressivePosition;
     public float velocite;
@@ -26,18 +27,20 @@ public class Navigation : MonoBehaviour {
     public bool slowDuringTurning = true;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         float valueH = Input.GetAxis("Horizontal");
         float valueV = Input.GetAxis("Vertical");
+        bool backward = Input.GetButton("GoingBackward");
+        float backwardFactor = 1;//va être mis à -1 si on appuie sur la touche de reculons
         Vector3 myVector;
-
-        myVector = new Vector3(1,0,0) * valueH;
+        //Debug.Log("H:" + valueH + "V:" + valueV);
+        myVector = new Vector3(1, 0, 0) * valueH;
         myVector += new Vector3(0, 0, 1) * valueV;
 
         myVector.Normalize();
@@ -47,6 +50,11 @@ public class Navigation : MonoBehaviour {
         directionBateau.transform.position = transform.position;
         directionBateau.transform.position += myVector;
 
+        if(backward)
+        {
+            backwardFactor = -1;
+        }
+
         //on est en train de faire un input sur le controller
         if ((valueH > deadZone || valueH < -deadZone) || (valueV > deadZone || valueV < -deadZone))
         {
@@ -55,38 +63,51 @@ public class Navigation : MonoBehaviour {
             testAngle.transform.LookAt(m_ProgressivePosition);
             float monAngleAvantRotation = Quaternion.Angle(transform.rotation, testAngle.transform.rotation);
 
-            if (velocite > speedMinToRotate)//si on ne peut pas trouner sur soi-même, on on vérifie qu'on va assez vite
+            if (Mathf.Abs(velocite) > speedMinToRotate)//si on ne peut pas trouner sur soi-même, on on vérifie qu'on va assez vite
             {
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, testAngle.transform.rotation, turnSpeed * Time.deltaTime);
             }
             else if (monAngleAvantRotation > angleAvance) //le joueur ne va pas assez vite pour tourner et il veut touner donc on l'accélère. 
             {
-                velocite += speed * Time.deltaTime;
+                velocite += speed * Time.deltaTime * backwardFactor;
             }
 
             float monAngleApresRotation = Quaternion.Angle(transform.rotation, testAngle.transform.rotation);
 
             if (monAngleApresRotation < angleAvance)//si on pointe vers le devant du bateau 
-                velocite += speed * Time.deltaTime;
+                velocite += speed * Time.deltaTime * backwardFactor;
             else if (slowDuringTurning)//si on est dans le cas
             {
-                velocite -= (0.75f * speedSlowing) * Time.deltaTime;
-                if (velocite < speedMinToRotate)
+                velocite -= (0.75f * speedSlowing) * Time.deltaTime * backwardFactor;
+                if (Mathf.Abs(velocite) < speedMinToRotate)
                 //si on ne peut pas tourner sur sois-meme et qu'on est rendu plus lent que la vitesse minimal on va annuler le ralentissement
                 {
-                    velocite += (0.75f * speedSlowing) * Time.deltaTime;
+                    velocite += (0.75f * speedSlowing) * Time.deltaTime * backwardFactor;
                 }
             }
 
         }
         else//on fait pas d'input donc on ralentit
-            velocite -= speedSlowing * Time.deltaTime;
+        {
+            if (velocite > 0)//si on est à 0 on ne va pas changer la vitesse
+            {
+                velocite -= speedSlowing * Time.deltaTime;
+            }
+            else if(velocite < 0)//on ralentit dans l'autre sens
+            {
+                velocite += speedSlowing * Time.deltaTime;
+            }
+        }
 
         if (velocite > speedMax)//on limite la vitesse
+        {
             velocite = speedMax;
+        }
+        else if (velocite < -speedMax)
+        {
+            velocite = -speedMax;
+        }
 
-        if (velocite < 0)//on peut juste avancer, on ne peut pas reculer
-            velocite = 0;
 
         transform.position += transform.forward * velocite;
     }
