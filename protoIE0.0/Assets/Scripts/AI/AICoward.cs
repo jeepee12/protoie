f54161095fb5127d19b83c15baa543dca685a6fc
Baltazar510 <devil.hunter510@gmail.com>
2016-02-29 13:58:27 -0500

    Refactor to use Inheritance with AI, added Coward AI
    Baked a new navMesh
    Changed how AI behavior are selected
    More OO approach for AI script
    Added a AI Template as AI Parent for Inheritance

---
+++ protoIE0.0/Assets/Scripts/AI/AICoward.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AICoward : AITemplate
+{
+    public float fearFactor = 50f;
+
+    public AICoward(Transform enemyTransform, Transform playerTransform, NavMeshAgent navAgent)
+    {
+        this.enemyTransform = enemyTransform;
+        this.playerTransform = playerTransform;
+        this.navAgent = navAgent;
+    }
+
+    public override void LookAt()
+    { // Look at something
+
+        Quaternion turnRotation = Quaternion.LookRotation(this.enemyTransform.transform.position - this.playerTransform.position);
+
+
+        this.enemyTransform.transform.rotation = Quaternion.Slerp(this.enemyTransform.transform.rotation, turnRotation, Time.deltaTime * this.damping);
+    }
+
+    public override void Move()
+    { // Move to a destination
+
+        Transform startTransform = this.enemyTransform;
+        this.enemyTransform.rotation = Quaternion.LookRotation(this.enemyTransform.position - this.playerTransform.position);
+
+        Vector3 runTo = this.enemyTransform.transform.position + this.enemyTransform.transform.forward * this.fearFactor;
+
+        NavMeshHit destination;
+        NavMesh.SamplePosition(runTo, out destination, 50, 1 << NavMesh.GetAreaFromName("Walkable"));
+
+        this.enemyTransform.position = startTransform.position;
+        this.enemyTransform.rotation = startTransform.rotation;
+
+        this.navAgent.destination = destination.position;
+    }
+
+    public override void Attack()
+    { // Attack something
+
+    }
+
+}
---
+++ protoIE0.0/Assets/Scripts/AI/EnemyAI.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum EnemySelection
+{
+    Default, Follower, Coward
+}
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public EnemySelection enemySelection;
+    public AITemplate enemyAI;
+
+    private void Start()
+    {
+        switch (enemySelection)
+        {
+            case EnemySelection.Default: 
+                enemyAI = new AIFollower(transform, GameObject.FindGameObjectWithTag("Player").transform, GetComponent<NavMeshAgent>());
+                break;
+            case EnemySelection.Follower:
+                enemyAI = new AIFollower(transform, GameObject.FindGameObjectWithTag("Player").transform, GetComponent<NavMeshAgent>());
+                break;
+            case EnemySelection.Coward:
+                enemyAI = new AICoward(transform, GameObject.FindGameObjectWithTag("Player").transform, GetComponent<NavMeshAgent>());
+                break;
+        }
+
+        enemyAI.enemyTransform = transform;
+        enemyAI.playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        enemyAI.navAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        enemyAI.playerDistance = Vector3.Distance(enemyAI.playerTransform.position, transform.position);
+
+        if (enemyAI.playerDistance > enemyAI.lookRange)
+        { // The player is too far away from the enemy
+            //Debug.Log("The player is too far away from the enemy");
+
+        } else
+        { // The player can be seen by the enemy
+            //Debug.Log("The player can be seen by the enemy");
+            enemyAI.LookAt();
+        }
+        
+        if (enemyAI.playerDistance <= enemyAI.attackRange)
+        { // The player is within attack range
+            //Debug.Log("The player is within attack range");
+            enemyAI.navAgent.Stop();
+            enemyAI.Attack();
+        }
+        else if (enemyAI.playerDistance <= enemyAI.chaseRange)
+        { // The player is within chasing range
+            //Debug.Log("The player is within chasing range");
+            enemyAI.navAgent.Resume();
+            enemyAI.Move();
+        }
+    }
+
+}
