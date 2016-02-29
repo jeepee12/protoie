408150c90075a4ca56eb4435b1818d71bba2f07f
spino <philippe_spino@hotmail.com>
2016-02-29 09:36:18 -0500

    Added Seagull_Flock_sound and Sound box.

---
+++ protoIE0.0/Assets/Scripts/WeaponDisplay/Seagull_Flock_Sound_trigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class Seagull_Flock_Sound_trigger : MonoBehaviour {
+
+    AudioSource Flocksound;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Flocksound = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider others)
+    {
+        if (others.CompareTag("Player"))
+            Flocksound.Play();
+        else
+            Flocksound.Pause();
+    }
+
+    void OnTriggerExit(Collider others)
+    {
+        if (others.CompareTag("Player"))
+            Flocksound.Stop();
+        else
+            Flocksound.Play();
+    }
+}
