41e0cf46b164bb81cfdf1c6f27dea6309159e987
Frédéric Carrier <dragon_eyes@live.fr>
2016-03-01 14:59:24 -0500

    Fix pull

--- protoIE0.0/Assets/Scripts/AI/AITemplate.cs
+++ protoIE0.0/Assets/Scripts/AI/AITemplate.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 
-public class AITemplate {
+public class AITemplate
+{
+
+    public bool melee = true;
     public float lookRange = 1000f;
-    public float chaseRange = 300f;
     public float attackRange = 10f;
+    public float chaseRange = 300f;
     public float damping = 0.8f;
+    public float fearFactor = 50f;
+    public float playerDistance = 0f;
+
     public Transform playerTransform;
     public Transform enemyTransform;
-    public float playerDistance = 0f;
+
     public NavMeshAgent navAgent;
 
     public virtual void LookAt() { } // Look at something
@@ -15,4 +21,11 @@ public class AITemplate {
     public virtual void Move() { } // Move to a destination
 
     public virtual void Attack() { } // Attack something
+
+    public virtual void ChaseAndAttack() { } // Check for chase and attack reach
+
+    public bool IsOutOfView()
+    {
+        return playerDistance > lookRange;
+    }
 }
