147a043ef68236c0b89339e60e7415b6b5f3921a
jeepee12 <jeepee.12@hotmail.com>
2016-03-04 11:07:13 -0500

    Le bateau se fait heal quand il est proche des iles(à toutes les 2 secondes pour le moment)

--- protoIE0.0/Assets/Scripts/LandObjective.cs
+++ protoIE0.0/Assets/Scripts/LandObjective.cs
@@ -3,16 +3,46 @@
 public class LandObjective : MonoBehaviour {
 
 	private MapGenerator mapGScript;
+    private PlayerStats player;
+    private float playerEntryTime;
+    public float healCoolDown = 2000.0f;
+    public int healAmount = 1;
 	
 	void Start () {
 		mapGScript = GameObject.FindGameObjectWithTag("MapGenerator").GetComponent<MapGenerator>();
-	}
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+    }
 
 	private void OnTriggerExit(Collider other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
-			mapGScript.StageIsCompleted();
-		}
+            playerEntryTime = 0;
+            mapGScript.StageIsCompleted();
+        }
+
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            playerEntryTime = Time.time;
+
+            //Debug.Log("TriggerEnter" + player.getHP());
+        }
+    }
+
+    void Update()
+    {
+        if(playerEntryTime > 0)
+        {
+            if(Time.time - playerEntryTime > healCoolDown)
+            {
+                player.AffectHP(healAmount);
+                playerEntryTime = Time.time;
+            }
+        }
+    }
 }
--- protoIE0.0/Assets/Scripts/PlayerStats.cs
+++ protoIE0.0/Assets/Scripts/PlayerStats.cs
@@ -1,23 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerStats : MonoBehaviour {
+public class PlayerStats : MonoBehaviour
+{
 
     public int StartHP;
+    private const int maxHP = 12;
 
+    //mettre prive quand les tests sont terminés
     public int HP;
-    
+
     public int getHP() { return HP; }
-    public int AffectHP(int diff) { return HP + diff; }
+    public void AffectHP(int diff)
+    {
+        HP = HP + diff;
+
+        if(HP > maxHP)
+        {
+            HP = maxHP;
+        }
+        else if(HP < 0)
+        {
+            //on ne veut pas que les hp du joueurs soient en bas de 0
+            HP = 0;
+        }
+    }
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         HP = StartHP;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
 }
