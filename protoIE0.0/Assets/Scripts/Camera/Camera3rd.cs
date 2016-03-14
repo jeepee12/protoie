749a590c33d744b6faa69da109974f9d1c5b1b61
Striker299 <daiglemartin@hotmail.com>
2016-03-14 19:33:24 -0400

    Changement a la camera 3iem personne.
    La camera ne devrais plus suivre le bateau a en donner mal au coeur

--- protoIE0.0/Assets/Scripts/Camera/Camera3rd.cs
+++ protoIE0.0/Assets/Scripts/Camera/Camera3rd.cs
@@ -3,16 +3,18 @@ using System.Collections;
 
 public class Camera3rd : MonoBehaviour {
 
-    public Vector3 CameraPreset1Position = new Vector3(2.0f, 28.0f, -110.0f);
-    public Vector3 CameraPreset1Rotation = new Vector3(55.0f, 0.0f, 0.0f);
+    public Vector3 CameraPreset1Position = new Vector3(0.0f, 28.0f, -110.0f);
 
     public Transform Bateau;
+
+    private Vector3 startPos =  Vector3.zero;
 
     // Use this for initialization
     void Start ()
     {
-        transform.rotation = Quaternion.Euler(CameraPreset1Rotation);
+        LateUpdate();
 
+        startPos = transform.position;
     }
 	
 	// Update is called once per frame
@@ -24,6 +26,9 @@ public class Camera3rd : MonoBehaviour {
         pos += Bateau.forward * CameraPreset1Position.z;
         pos += Bateau.up * CameraPreset1Position.y;
         pos += Bateau.right * CameraPreset1Position.x;
+
+        if (startPos != Vector3.zero)
+            pos.y = startPos.y;
 
         transform.position = pos;
 
---
+++ protoIE0.0/Assets/Scripts/FollowObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+    public GameObject toFollow;
+
+    private Vector3 startPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        startPos = toFollow.transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 pos = toFollow.transform.position;
+
+        pos.y = startPos.y;
+
+        transform.position = pos;
+
+        transform.rotation = toFollow.transform.rotation;
+	}
+}
